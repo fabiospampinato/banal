fbf3dc2fa2d93063e015d745fec0a961867110e7
Fabio Spampinato <spampinabio@gmail.com>
2023-12-10 01:07:51 +0100

    Ensuring es2021 is listed as a valid target

--- src/types.ts
+++ src/types.ts
@@ -5,7 +5,7 @@ type Options = {
   /* ESBUILD OPTIONS */
   format?: 'iife' | 'cjs' | 'esm',
   platform?: 'browser' | 'node' | 'neutral',
-  target?: 'es2016' | 'es2017' | 'es2018' | 'es2019' | 'es2020' | 'esnext',
+  target?: 'es2016' | 'es2017' | 'es2018' | 'es2019' | 'es2020' | 'es2021' | 'esnext',
   /* BANAL OPTIONS */
   module?: string[] | string,
   entry?: string
